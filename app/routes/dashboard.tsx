8b247b85567cbaed37a7d8c5a7b190a6f543bf8a
Alexey <alexei20293@yahoo.com>
2023-05-21 00:16:06 +0300

    add(page): dashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,11 +1,24 @@
+import { LoaderFunction, V2_MetaFunction, json } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
 import { Layout } from "~/components/layout";
+import { getUser } from "~/utils/session.server";
 
-
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await getUser(request);
+  console.log(user);
+  return json({ user });
+};
 export default function Index() {
+  const { user }: { user: User } = useLoaderData();
   return (
-    <Layout>
-      <h1>Dashboard</h1>
-    </Layout>
- 
+    <>
+      <Layout user={user}>
+        <Outlet />
+        <h1>Home page</h1>
+      </Layout>
+    </>
   );
 }
