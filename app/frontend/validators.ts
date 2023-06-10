7ef23583da36d569c711e12f5d98d7e5ab8d22cf
Alexey <alexei20293@yahoo.com>
2023-06-10 16:40:31 +0300

    add frontend and backend validators

--- app/frontend/validators.ts
+++ app/frontend/validators.ts
@@ -1,4 +1,4 @@
-export const validateEmail = (email: string): string | undefined => {
+export const validateEmailFE = (email: string): string | undefined => {
   const validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (!email.length || !validRegex.test(email)) {
@@ -7,12 +7,12 @@ export const validateEmail = (email: string): string | undefined => {
   // if (!email.length) return `Please enter a value`;
 };
 
-export const validatePassword = (password: string): string | undefined => {
+export const validatePasswordFE = (password: string): string | undefined => {
   if (password.length < 5) {
     return "Please enter a password that is at least 5 characters long";
   }
 };
 
-export const validateName = (name: string): string | undefined => {
+export const validateNameFE = (name: string): string | undefined => {
   if (!name.length) return `Please enter a value`;
 };
