fce91ff763e19837851e8aa1ee70efc1c5130304
Alexey <alexei20293@yahoo.com>
2023-06-10 21:20:28 +0300

    add types login and register form

--- app/types/types.ts
+++ app/types/types.ts
@@ -2,3 +2,14 @@ interface User {
   id: string;
   email: string;
 }
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
+type RegisterForm = {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+};
