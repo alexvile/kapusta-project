8c38787c3ad0e2d5e4926deb7fe9c226232e93c8
Alexey <alexei20293@yahoo.com>
2023-06-25 17:33:01 +0300

    add(temporary): extended colors at tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,34 +2,35 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      'accent': {
-        light: '#fb7c2f33',
-        DEFAULT: '#FB7C2F',
-      },
-      'main': '#000000',
-      'secondary': '#52555F',
-      'placeholder': '#A6ABB9',
-      'error': '#EB5757',
-      'light': '#ffffff',
-      'expenses': '#E7192E',
-      'income': '#407946',
-      'bg-main': '#F5F6FB',
-      'bg-input': '#F6F7FB',
-      'chart': {
-        DEFAULT: '#FF751D',
-        light: '#FFDAC0'
-      }
-    },
     extend: {
+      colors: {
+        accent: {
+          light: "#fb7c2f33",
+          DEFAULT: "#FB7C2F",
+        },
+        main: "#000000",
+        secondary: "#52555F",
+        placeholder: "#A6ABB9",
+        error: "#EB5757",
+        light: "#ffffff",
+        expenses: "#E7192E",
+        income: "#407946",
+        "bg-main": "#F5F6FB",
+        "bg-input": "#F6F7FB",
+        chart: {
+          DEFAULT: "#FF751D",
+          light: "#FFDAC0",
+        },
+      },
       backgroundImage: {
-        'home-page': "url('../public/img/bg-desktop.png')",
+        "home-page": "url('../public/img/bg-desktop.png')",
       },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
       },
     },
+    plugins: [],
   },
-  plugins: [],
-}
+};
 
+// todo: remove extend at production
