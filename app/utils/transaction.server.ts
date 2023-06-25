895db1e7fac369b1fcf8e8f12fa72bea2f0e933f
Alexey <alexei20293@yahoo.com>
2023-06-25 22:00:13 +0300

    update(backend): transaction (expenses)

--- app/utils/transaction.server.ts
+++ app/utils/transaction.server.ts
@@ -1,11 +1,31 @@
 import { db } from "./db.server";
 import type { Expense as IExpense } from "@prisma/client";
 
+// const {
+//   createRequestHandler,
+// } = require("@remix-run/express");
+
+// app.all(
+//   "*",
+//   createRequestHandler({
+//     getLoadContext(req, res) {
+//       // this becomes the loader context
+//       return { expressUser: req.user };
+//     },
+//   })
+// );
+
+// todo - add Middleware at all routes !!
+
 // export const getAllExpense = async () => {
 //   return await db.expense.findMany({});
 // };
 
 // todo - get for month and other period
+
+// todo - frontend or/and backend validation to expenses !!!
+
+// todo - add modal component form remix 3 tutorial
 
 export const getAllExpensesByUserId = async (userId: string) => {
   return await db.expense.findMany({
@@ -27,29 +47,6 @@ export const getExpenseByIdAndUserId = async (
     },
   });
   // todo: select optimal db.expense.findOne or findUnique or findFirstOrThrow or findUniqueOrThrow
-};
-
-// todo: can we use 1 operation to find, draw interface and update ??
-// todo: update only by Expense ID or including user ID ???
-
-export const updateExpenseById = async ({
-  id,
-  createdTime,
-  description,
-  type,
-  value,
-}: Pick<IExpense, "id" | "createdTime" | "description" | "type" | "value">) => {
-  return await db.expense.update({
-    where: {
-      id,
-    },
-    data: {
-      description,
-      createdTime,
-      type,
-      value,
-    },
-  });
 };
 
 export const createExpense = async ({
@@ -76,3 +73,32 @@ export const createExpense = async ({
     },
   });
 };
+
+// todo: can we use 1 operation to find, draw interface and update ??
+// todo: update only by Expense ID or including user ID ???
+
+export const updateExpenseById = async ({
+  id,
+  createdTime,
+  description,
+  type,
+  value,
+}: Pick<IExpense, "id" | "createdTime" | "description" | "type" | "value">) => {
+  return await db.expense.update({
+    where: {
+      id,
+    },
+    data: {
+      description,
+      createdTime,
+      type,
+      value,
+    },
+  });
+};
+
+export const deleteExpenseById = async (id: string) => {
+  return await db.expense.delete({ where: { id } });
+};
+
+// todo: catch an error boundaries
