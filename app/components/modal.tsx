52c5941db7e10743a5bc353fb7b8d721980f0932
Alexey <alexei20293@yahoo.com>
2023-06-25 17:32:32 +0300

    add(component): modal and popup

---
+++ app/components/modal.tsx
@@ -0,0 +1,76 @@
+// NOTE FORM AUTHOR ***
+// TO CLOSE MODAL
+// If we use modal as separate route we should use navigate to return back.
+// But if we don't use separate route we should use callback to change boolean isOpen.
+// We use Modal or Popup. Modal - for separate route, Popup alerts etc.
+import { Portal } from "./portal";
+import { useNavigate } from "@remix-run/react";
+
+type ConditionalProps =
+  | {
+      type: "modal";
+      onClose?: never;
+    }
+  | {
+      type: "popup";
+      onClose: () => void;
+    };
+
+interface props {
+  children: React.ReactNode;
+  isOpen: boolean;
+  ariaLabel?: string;
+  className?: string;
+}
+
+export const Modal: React.FC<props & ConditionalProps> = ({
+  type,
+  children,
+  isOpen,
+  ariaLabel,
+  className,
+  onClose,
+}) => {
+  const navigate = useNavigate();
+  if (!isOpen) return null;
+  console.log(ariaLabel);
+
+  //  todo: create not modal route, but popup
+  return (
+    <Portal wrapperId="modal">
+      <div
+        className="fixed inset-0 overflow-y-auto bg-gray-600 bg-opacity-80"
+        aria-labelledby={ariaLabel ?? "modal-title"}
+        role="dialog"
+        aria-modal="true"
+        onClick={() => {
+          type === "modal"
+            ? navigate(
+                "/dashboard/6484b3d1c1dc8cc1be2c6234/transactions/expenses"
+              )
+            : onClose();
+        }}
+
+        // todo: normal navigate
+      ></div>
+      <div className="fixed inset-0 pointer-events-none flex justify-center items-center max-h-screen overflow-scroll">
+        <div
+          className={`${className} p-4 bg-gray-200 pointer-events-auto max-h-screen md:rounded-xl`}
+        >
+          {children}
+          <button
+            onClick={() => {
+              type === "modal"
+                ? navigate(
+                    "/dashboard/6484b3d1c1dc8cc1be2c6234/transactions/expenses"
+                  )
+                : onClose();
+            }}
+          >
+            CLOSE
+          </button>
+        </div>
+      </div>
+    </Portal>
+  );
+};
