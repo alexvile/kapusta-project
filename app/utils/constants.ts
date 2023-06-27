2974e695ab9be007ac6ce9faef55ff00f1e50710
Alexey <alexei20293@yahoo.com>
2023-06-27 22:08:42 +0300

    add(file): constants

---
+++ app/utils/constants.ts
@@ -0,0 +1,9 @@
+export const ExpenseKinds = [
+  { name: "Rent", value: "RENT" },
+  { name: "Communal", value: "COMMUNAL" },
+  { name: "Marketing", value: "MARKETING" },
+  { name: "Consumables", value: "CONSUMABLES" },
+  { name: "Other", value: "OTHER" },
+];
+
+//   todo - how to synchronize with db ???
