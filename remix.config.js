d10b01698640636ae07af5d6cce081edf7f48cd3
Alexey <alexei20293@yahoo.com>
2023-07-01 12:48:33 +0300

    update settings

--- remix.config.js
+++ remix.config.js
@@ -9,11 +9,13 @@ module.exports = {
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
+  serverModuleFormat: "cjs",
+  tailwind: true,
   future: {
-    unstable_tailwind: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
+    v2_headers: true,
   },
 };
