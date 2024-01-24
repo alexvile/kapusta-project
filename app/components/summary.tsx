35d0ef7f25dc0539a5cd3f5c9da8c1affacd4022
Alexey <alexei20293@yahoo.com>
2024-01-24 21:28:50 +0200

    add(styles): summary block

--- app/components/summary.tsx
+++ app/components/summary.tsx
@@ -1,4 +1,5 @@
 import { calculateByMonths } from "~/helpers/monthsCalculator";
+import { formatToPrice } from "~/helpers/priceFormat";
 import { MONTH_NAMES } from "~/utils/constants";
 
 export const Summary = ({ transactions }: { transactions: any }) => {
@@ -6,12 +7,20 @@ export const Summary = ({ transactions }: { transactions: any }) => {
   const groupsByMonths = calculateByMonths(transactions);
 
   return (
-    <div className="bg-white outline p-2 rounded border mt-2">
-      Summary
+    <div className="rounded-t-2xl rounded-br-2xl overflow-hidden min-w-[230px]">
+      <h3 className="bg-mainBg mb-0.5 text-center font-roboto text-main text-label tracking-small font-bold uppercase py-3">
+        Summary
+      </h3>
       <ul>
         {groupsByMonths.map((el) => (
-          <li key={el.month}>
-            {MONTH_NAMES[el.month]} &nbsp; {el.total}
+          <li
+            key={el.month}
+            className="bg-mainBg mb-0.5 lg:mb-1 py-3 px-5 gap-8 last:mb-0 font-roboto text-secondary text-label tracking-medium uppercase flex items-center justify-between"
+          >
+            <span>{MONTH_NAMES[el.month]}</span>
+            <span>
+              {formatToPrice(el.total, { symbolType: "narrowSymbol" })}
+            </span>
           </li>
         ))}
       </ul>
