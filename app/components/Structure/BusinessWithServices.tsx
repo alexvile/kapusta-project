be78795b0a3352ba9bda550682fa8703aeb2d94d
Alexey <alexei20293@yahoo.com>
2024-05-26 09:20:41 +0300

    add(feature): expand/close all accordions

--- app/components/Structure/BusinessWithServices.tsx
+++ app/components/Structure/BusinessWithServices.tsx
@@ -1,68 +1,77 @@
 import { IBusinessWithServices, IOpenModal } from "~/types/types";
 import { Service } from "./Service";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Service as IService } from "@prisma/client";
 import { Button } from "../Layout/Button";
 import { Icon } from "../Layout/Icon";
+import { AccordionBtn } from "../Layout/AccordionBtn";
+import { AccordionContent } from "../Layout/AccordionContent";
 
-type BusinessProps = IBusinessWithServices & { openModal: IOpenModal };
+type BusinessProps = IBusinessWithServices & {
+  openModal: IOpenModal;
+  allAccOpen: Boolean;
+};
 
 export const BusinessWithServices = ({ ...props }: BusinessProps) => {
   // todo REFACTOR!!!!!
-  const { name, notes, services, id, openModal } = props;
-  const [showCategories, setShowCategories] = useState(true);
+  const { name, notes, services, id, openModal, allAccOpen } = props;
+  const [showCategories, setShowCategories] = useState(allAccOpen);
   const toggleCategories = () => {
     setShowCategories(!showCategories);
   };
+  useEffect(() => {
+    setShowCategories(allAccOpen);
+  }, [allAccOpen]);
 
-  //  if no icon use random icon
   return (
     <li className="pl-3 my-2">
-      <div className="flex items-center justify-start gap-1">
-        <div>{name}</div>
-        <div>{notes}</div>
-        {services.length > 0 && (
-          <Button onPress={toggleCategories} style="round">
-            <span
-              className={`transition-transform block ${
-                showCategories ? "rotate-180" : ""
-              }`}
-            >
-              <Icon name="chevron-down" />
-            </span>
+      {/* <details className="transition-all">
+        <summary>Details</summary>
+        Something small enough to escape casual notice.
+      </details> */}
+
+      <div className="flex items-center justify-between border-b-2 pb-2">
+        <div>
+          <h3 className="font-bold">{name}</h3>
+          {/* use notes as tooltip */}
+          <div>{notes}</div>
+        </div>
+
+        {/* actions */}
+        <div className="flex">
+          {services.length > 0 && (
+            <AccordionBtn isOpen={showCategories} handler={toggleCategories} />
+          )}
+          <Button
+            style="action"
+            ariaLabel="Edit business"
+            onPress={() =>
+              openModal({
+                intent: "edit-business",
+                target: id,
+                fields: { name, notes },
+              })
+            }
+          >
+            <Icon name="edit" />
           </Button>
-        )}
-        <Button
-          style="round"
-          ariaLabel="Edit business"
-          onPress={() =>
-            openModal({
-              intent: "edit-business",
-              target: id,
-              fields: { name, notes },
-            })
-          }
-        >
-          <Icon name="edit" />
-        </Button>
-        <Button
-          style="round"
-          ariaLabel="Add service"
-          onPress={() => openModal({ intent: "create-service", target: id })}
-        >
-          <Icon name="plus" />
-        </Button>
+          <Button
+            style="action"
+            ariaLabel="Add service"
+            onPress={() => openModal({ intent: "create-service", target: id })}
+          >
+            <Icon name="plus" />
+          </Button>
+        </div>
       </div>
       {services.length > 0 && (
-        <ul
-          className={` ml-6 transition-transform ${
-            showCategories ? "max-h-auto" : "max-h-0 overflow-hidden"
-          }`}
-        >
-          {services.map((s: IService) => (
-            <Service key={s.id} {...s} openModal={openModal} />
-          ))}
-        </ul>
+        <AccordionContent isOpen={showCategories}>
+          <ul>
+            {services.map((s: IService) => (
+              <Service key={s.id} {...s} openModal={openModal} />
+            ))}
+          </ul>
+        </AccordionContent>
       )}
     </li>
   );
